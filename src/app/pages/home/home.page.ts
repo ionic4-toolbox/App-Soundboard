0fdb0382585c8f8df5350743abb1a618a18d64fd
Welkson Ramos <welksonr@hotmail.com>
2019-02-06 11:32:42 -0200

    Update home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SoundsDataService } from './../../services/sounds-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  songs: any;
+  constructor(private soundsData: SoundsDataService) { }
+
+  ngOnInit() {
+    this.soundsData.load().subscribe(data => { this.songs = data.results[0].songs; console.log(this.songs) }
+    );
+  }
 
 }
